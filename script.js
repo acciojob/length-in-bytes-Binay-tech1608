04973884fed7354bd870c5eaf0b1d1de27a3ddee
Binay Mann <82080713+Binay-tech1608@users.noreply.github.com>
2026-04-11 10:26:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const byteSize = (str) => {
   // write your code here
-	return str.length;
+	return new TextEncoder().encode(str).length;
 };
+
 
 // Do not change the code below
 const str = prompt("Enter some string.");
